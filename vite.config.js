bbc7d74d22bb487d3076f13e987ca38592c7df79
kingsley Ebuka <kingsleydunu@gmail.com>
2025-10-07 21:46:19 +0100

    fixed

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "./", // 👈 Needed for Netlify
+  base: "/", // 👈 Needed for Netlify
   build: {
     chunkSizeWarningLimit: 2000,
   },
